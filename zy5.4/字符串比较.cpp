461c3d9380677372243709a840dc9092e299cb75
PhoebeQst <867768011@qq.comPqsttcy>
2023-05-04 12:35:38 +0800

    add zy5.4/

--- zy5.4/字符串比较.cpp
+++ zy5.4/字符串比较.cpp
@@ -3,13 +3,14 @@ using namespace std;
 int main(){
 	char a[10000]={'\0'},b[10000]={'\0'};
 	int p=0;
-	scanf("%[^\n]",a);;
-	scanf("%[^\n]",b);
+	cin.getline(a, 10000);
+	cin.getline(b, 10000);
 	for(int i=0;a[i]!='\0';i++){
+		int t=i;
 		if(a[i]==b[0]){
 			for(int j=1;b[j]!='\0';j++){
-				int t=i;
-				if(b[j]!=a[++t]){
+				t++;
+				if(b[j]!=a[t]){
 					break;
 				}if(b[j+1]=='\0'){
 					cout<<i<<" ";
